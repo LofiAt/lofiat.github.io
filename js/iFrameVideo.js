46bdfebc14b6173399a8a623c6e6085278067432
LofiAt <84640942+LofiAt@users.noreply.github.com>
2022-08-24 23:23:11 +0100

    Update iFrameVideo.js

--- js/iFrameVideo.js
+++ js/iFrameVideo.js
@@ -176,6 +176,7 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady() {
   console.log("hey Im ready");
+  player.playVideo();
   if (document.getElementById("volumeStateToggle").innerHTML === '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>') {
     document.getElementById("volumeStateToggle").ariaLabel = "hear";
     player.unMute();
