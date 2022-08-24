138abc01ea2c9b743caf954863d2ceda8c3136ba
LofiAt <84640942+LofiAt@users.noreply.github.com>
2022-08-24 02:27:32 +0100

    Update iFrameVideo.js

--- js/iFrameVideo.js
+++ js/iFrameVideo.js
@@ -217,14 +217,23 @@ document.getElementById("volumeStateToggle").addEventListener('click', function(
     vol50 = function(){}; // kill it as soon as it was called
     player.unMute();
     document.getElementById('volumeSlider').value = 50;
+    player.setVolume(50);
   };
 
   if(player.isMuted()) {
-    vol50()
+    vol50();
     player.unMute();
+    document.getElementById('volumeStateToggle').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>';
     document.getElementById('volumeSlider').value = vol;
+    if(vol<1) {
+      player.unMute();
+      document.getElementById('volumeSlider').value = 50;
+      player.setVolume(50);
+      document.getElementById('volumeStateToggle').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>';
+    }
   } else {
     player.mute();
+    document.getElementById('volumeStateToggle').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-x"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><line x1="23" y1="9" x2="17" y2="15"></line><line x1="17" y1="9" x2="23" y2="15"></line></svg>';
     document.getElementById('volumeSlider').value = 0;
   }
 });
