f41fac4bd9028c9205e2ff0c2b865d816afe1263
LofiAt <84640942+LofiAt@users.noreply.github.com>
2022-11-17 20:37:28 +0000

    Update saveSpaces.js

--- js/saveSpaces.js
+++ js/saveSpaces.js
@@ -1,19 +1,3 @@
-function saveTheSpace () {
-    // Get the element
-    var elem = document.querySelector('.favouriteSpaceWrapper');
-
-    // Create a copy of it
-    var clone = elem.cloneNode(true);
-
-    // Update the ID and add a class
-    clone.class = 'favouriteSpaceWrapper';
-    clone.classList.add('text-large');
-
-    // Inject it into the DOM
-    elem.after(clone);
-}
-
-function deleteSavedSpace() {
-    var element = document.getElementById("myElementID");
-    element.parentNode.removeChild(element);
+function deleteSavedSpace(idName) {
+    document.getElementById(idName).remove()
 }
