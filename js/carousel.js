f4be05f21904e5ed0a9cc8dd01a8afd2688ea2ca
LofiAt <84640942+LofiAt@users.noreply.github.com>
2023-03-06 04:04:57 +0000

    Change Carousel Button Scroll

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,17 @@
 function moveCarouselLeft() {
-    document.getElementById("spacesCategoriesCarousel").scrollTo(0,0);
+    if (document.getElementById("spacesCategoriesCarousel").scrollLeft > 252) {
+        document.getElementById("spacesCategoriesCarousel").scrollTo(252,0);
+    }
+    else {
+        document.getElementById("spacesCategoriesCarousel").scrollTo(0,0);
+    }
 }
 
 function moveCarouselRight() {
-    document.getElementById("spacesCategoriesCarousel").scrollTo(244,0);
+    if (document.getElementById("spacesCategoriesCarousel").scrollLeft < 252) {
+        document.getElementById("spacesCategoriesCarousel").scrollTo(252,0);
+    }
+    else {
+        document.getElementById("spacesCategoriesCarousel").scrollTo(376,0);
+    }
 }
